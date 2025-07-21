24ff9d9d2294e41ce1b75f1979c30fe36273061f
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-07-21 21:19:43 +0800

    editing User.java

--- Admin.java
+++ Admin.java
@@ -1,3 +1,8 @@
-public class Admin  extends User{
+public class Admin extends User{
+
+    public Admin(String userId, String name, String email, String password) {
+        super(userId, name, email, password);
+       
+    }
     
 }
