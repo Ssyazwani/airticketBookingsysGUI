2ccb476bf5d66d1ad81296af8ae91e695d6ca5a1
Ssyazwani <sitisyazwanibtemaziz@gmail.com>
2025-07-24 23:32:00 +0800

    adding the gui, need to add in the interfaces/buttons to test it works, objects can create without gui

---
+++ MainGui.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class MainGui  extends JFrame{
+    private JTextField textUserId;
+    private JPasswordField textPassword;
+    private JButton buttonLogin;
+    private JLabel lblStatus;
+    
+}
--- main.java
+++ main.java
@@ -7,8 +7,15 @@ public class Main {
             new Airline("Japan Airlines", 10, 10, 10),
             new Airline("Electrical Airlines", 10, 10, 10),
             new Airline("Republic of Ways Airlines", 10, 10, 10),
-
         };
+
+        User[] users = {
+             new User("awesome", "Syaz", "awesomepossum@gmail.com", "dumbo123"),
+        };
+
+       // Ticket[] tickets ={
+       //     new Ticket("001", "Syaz", getFlight("Singapore Airline"), "A01",100,"Approved"),
+       // };
         
         
 
